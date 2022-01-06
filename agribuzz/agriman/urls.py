15ebc5e4e07eeca09708eaa6cbcdaf80b269eab0
DKS007 <darpankumarsingworld42@gmail.com>
2022-01-07 01:31:05 +0530

    second commit

--- agribuzz/agriman/urls.py
+++ agribuzz/agriman/urls.py
@@ -5,4 +5,8 @@ from .import views
 urlpatterns = [
 	
 	path('', views.home, name = 'Home'),
+	path('signin/', views.signIn, name = 'Signin'),
+	path('signup/', views.signUp, name = 'Signup'),
+	path('farmermarket/', views.farmerMarket, name = 'Farmermarket'),
+	path('farmerkit/', views.farmerKit, name = 'Farmerkit'),
 ]
--- agribuzz/agriman/views.py
+++ agribuzz/agriman/views.py
@@ -6,4 +6,28 @@ from django.http import HttpResponse
 def home(request):
 
 	context = {}
-	return render(request, 'base.html', context)
+	return render(request, 'home.html', context)
+
+def signUp(request):
+
+	context = {}
+	return render(request, 'signup.html', context)
+
+
+
+def signIn(request):
+
+	context = {}
+	return render(request, 'signin.html', context)
+
+
+def farmerKit(request):
+
+	context = {}
+	return render(request, 'farmer_kit.html', context)
+
+
+def farmerMarket(request):
+
+	context = {}
+	return render(request, 'farmer_market.html', context)
